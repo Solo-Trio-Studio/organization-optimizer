5c47b3831da8f08794bdce91d8fc9d026de7b19a
ietzel <67884277+ietzel@users.noreply.github.com>
2025-04-17 12:21:04 -0400

    Update main.cbl

--- main.cbl
+++ main.cbl
@@ -2,5 +2,8 @@ IDENTIFICATION DIVISION.
 PROGRAM-ID. IDSAMPLE.
 ENVIRONMENT DIVISION.
 PROCEDURE DIVISION.
-    DISPLAY 'To make a business/partnership/company/corporation, make a google firebase host.'.
+    DISPLAY 'To make a business/partnership/company/corporation:'.
+    DISPLAY 'Make a summary of the collective'.
+    DISPLAY 'Designate board of directors roles (R&D, Revenue, Management/Administration, Purchasing, Production)'.
+    DISPLAY 'Make a google firebase host'.
     STOP RUN.
